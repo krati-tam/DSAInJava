d2c9f27abda7d3872df928d3d73887b93e6a2604
Krati Tamrakar <82703744+krati-tam@users.noreply.github.com>
2024-01-14 22:51:47 +0530

    Update and rename Longest Common Subsequence to Longest Common Subsequence.java

--- Hashing/Longest Common Subsequence.java
+++ Hashing/Longest Common Subsequence.java
@@ -1,3 +1,15 @@
+https://leetcode.com/contest/weekly-contest-380/problems/find-beautiful-indices-in-the-given-array-i/
+
+/*Example 1:
+
+Input: s = "isawsquirrelnearmysquirrelhouseohmy", a = "my", b = "squirrel", k = 15
+Output: [16,33]
+Explanation: There are 2 beautiful indices: [16,33].
+- The index 16 is beautiful as s[16..17] == "my" and there exists an index 4 with s[4..11] == "squirrel" and |16 - 4| <= 15.
+- The index 33 is beautiful as s[33..34] == "my" and there exists an index 18 with s[18..25] == "squirrel" and |33 - 18| <= 15.
+Thus we return [16,33] as the result.
+*/
+
 1. brute force approach - by sorting the elemnets of array 
 Suppose example - arr[]= {1,3,2,4,9,10}
                 output = 4     Because 1, 2, 3, 4 are in consecutive order and make the largest subsequence.
